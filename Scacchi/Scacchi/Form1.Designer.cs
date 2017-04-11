dd38082c61a7ebeace3d7c4995977c61180d37b6
Tony Fava <tony.fava@edu.itspiemonte.it>
2017-04-11 10:50:04 +0200

    Primo commit

---
+++ Scacchi/Scacchi/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Scacchi {
+    partial class Form1 {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSize = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.Triedmove = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dimensioni scacchiera";
+            // 
+            // txtSize
+            // 
+            this.txtSize.Location = new System.Drawing.Point(131, 6);
+            this.txtSize.Name = "txtSize";
+            this.txtSize.Size = new System.Drawing.Size(100, 20);
+            this.txtSize.TabIndex = 1;
+            this.txtSize.Text = "8";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(237, 4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Triedmove
+            // 
+            this.Triedmove.AutoSize = true;
+            this.Triedmove.Location = new System.Drawing.Point(358, 9);
+            this.Triedmove.Name = "Triedmove";
+            this.Triedmove.Size = new System.Drawing.Size(0, 13);
+            this.Triedmove.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 340);
+            this.Controls.Add(this.Triedmove);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtSize);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Tour del Cavaliere";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSize;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label Triedmove;
+    }
+}
+
---
+++ Scacchi/Scacchi/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scacchi {
+    public partial class Form1 : Form {
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        int BoardSize; //grandezza scacchiera
+        int[,] Offset = new int[8, 2]
+            {
+                {+2,+1}, {+1,+2},
+                {-1,+2}, {-2,+1},
+                {-2,-1}, {-1,-2},
+                {+1,-2}, {+2,-1}
+            };
+
+        private void btnStart_Click(object sender, EventArgs e) {
+            //controlla se non ci sono eccezioni in input
+            try {
+                BoardSize = int.Parse(txtSize.Text); //converte l'input
+            } catch {
+                BoardSize = 8; //default size
+                txtSize.Text = BoardSize.ToString(); //stampa default size nell'oggetto
+            }
+            int[,] Board = new int[BoardSize, BoardSize]; //dichiara una matrice scacchiera
+            
+            //posizione cavallo
+            Position InitialPosition;
+            InitialPosition.row = 0;
+            InitialPosition.col = 0;
+
+            //mosse fatte
+            int DoneMoves = 0,
+               TriedMoves = 0;
+
+            //posizione corrente
+            Position CurrentPosition = InitialPosition;
+            MarkMove(Board, CurrentPosition, ref DoneMoves); //segna sulla matrice il movimento fatto
+            MoveHorse(Board, CurrentPosition, DoneMoves); //muove il cavallo 
+            ShowBoard(Board);
+        }
+
+        //muove il cavallo, ma per sicurezza il contatore delle mosse viene passato by value
+        private bool MoveHorse(int[,] Board, Position CurrentPosition, int DoneMoves) {
+            if (DoneMoves == BoardSize * BoardSize)
+                return true;
+            
+
+            for(int i=0;i<8;i++)
+            {
+                Position NextPosition = CurrentPosition;
+                NextPosition.col += Offset[i, 0];
+                NextPosition.row += Offset[i, 1];
+                if(IsValid(Board, NextPosition))
+                {
+                    MarkMove(Board, NextPosition, ref DoneMoves);
+                    if(MoveHorse(Board, NextPosition, DoneMoves))
+                        return true;
+                    UnMarkMove(Board, NextPosition, ref DoneMoves);
+                }
+            }
+            return false;
+        }
+
+        //segna sulla matrice il movimento fatto
+        private void MarkMove(int[,] Board, Position CurrentPosition, ref int DoneMoves) {
+            DoneMoves++;
+            Board[CurrentPosition.row, CurrentPosition.col] = DoneMoves;
+            TriedMoves++;
+
+        }
+
+        //toglie dalla matrice il movimento fatto
+        private void UnMarkMove(int[,] Board, Position CurrentPosition, ref int DoneMoves) {
+            Board[CurrentPosition.row, CurrentPosition.col] = 0;
+            DoneMoves--;
+        }
+
+        private void ShowBoard(int[,] Board)
+        {
+            for(int r=0;r<BoardSize;r++)
+            {
+                for(int c=0;c<BoardSize;c++)
+                {
+                    string TextBoxName = "txt" + r.ToString() + "_" + c.ToString();
+                    TextBox t = (TextBox) this.Controls[TextBoxName];
+                    if (t == null)
+                    {
+                        t = new TextBox();
+                        t.Name = TextBoxName;
+                        t.Size = new Size(26, 22);
+                        t.Location = new Point(10 + c * 26, 50 + r * 22);
+                        this.Controls.Add(t);
+                    }
+                    if (Board[r, c] == 0)
+                    {
+                        t.Text = "";
+                        t.BackColor = Color.White;
+                    }
+                    else
+                    {
+                        t.Text = String.Format("{0:D2}", Board[r, c]);
+                        t.BackColor = Color.Aquamarine;
+                    }
+                }
+            }
+            Application.DoEvents();
+        }
+
+        private bool IsValid(int [,] Board, Position p)
+        {
+            if (p.row < 0 || p.row >= BoardSize || p.col < 0 || p.col >= BoardSize)
+                return false;
+            if (Board[p.row, p.col] != 0)
+                return false;
+            return true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+    }
+
+
+
+    //tipo posizione cavallo
+    struct Position {
+        public int row;
+        public int col;
+    }
+}
