f8792dcdbe805624c624a1bfc4ef544756fe4fb2
Tony Fava <tony.fava@edu.itspiemonte.it>
2017-04-24 11:32:46 +0200

    Aggiornamento mano utente coplit senza controlli

--- BlackJack/BlackJack/BlackJack/Form1.cs
+++ BlackJack/BlackJack/BlackJack/Form1.cs
@@ -17,8 +17,8 @@ namespace BlackJack
             InitializeComponent();
         }
         Random mazzo = new Random();
-        int mano = 1;
-
+        int mano=0,punteggio;
+        public int a1, a2, a3, a4;
         private void programmerToolStripMenuItem_Click(object sender, EventArgs e)
         {
             MessageBox.Show("By Fava Tony");
@@ -27,33 +27,44 @@ namespace BlackJack
         private void Form1_Load(object sender, EventArgs e)
         {
 
+
         }
 
         private void btnChiama_Click(object sender, EventArgs e)
         {
-            
-            mano++;
-            if (mano == 1)
-            {
-                int a1, a2,punteggio;
-                a1 = mazzo.Next(1, 11);
-                a2 = mazzo.Next(1, 11);
-                punteggio = a1 + a2;
-                lblplayer1m.Text = a1.ToString();
-                lblplayer2m.Text = a2.ToString();
-                lblRisultato.Text = punteggio.ToString();
-            }
+           
+                mano++;
+                if (mano == 1)
+                {
 
-             if (mano == 2)
-             {
-                    int a3,punteggio;
+                    a1 = mazzo.Next(1, 11);
+                    a2 = mazzo.Next(1, 11);
+                    punteggio = a1 + a2;
+                    lblplayer1m.Text = a1.ToString();
+                    lblplayer2m.Text = a2.ToString();
+                    lblRisultato.Text = punteggio.ToString();
+                }
+
+                if (mano == 2)
+                {
+
                     a3 = mazzo.Next(1, 11);
                     lblplayer3m.Text = a3.ToString();
-                    punteggio = Convert.ToInt32(lblplayer1m.Text)+Convert.ToInt32(lblplayer2m.Text)+a3;
+                    punteggio = Convert.ToInt32(lblplayer1m.Text) + Convert.ToInt32(lblplayer2m.Text) + a3;
                     lblRisultato.Text = punteggio.ToString();
 
-             }
+                }
+                if (mano == 3)
+                {
+
+                    a4 = mazzo.Next(1, 11);
+                    lblplayer4m.Text = a4.ToString();
+                    punteggio = Convert.ToInt32(lblplayer1m.Text) + Convert.ToInt32(lblplayer2m.Text) + Convert.ToInt32(lblplayer3m.Text) + a4;
+                    lblRisultato.Text = punteggio.ToString();
+
+                }
             
+
         }
 
         private void lblPunteggio_Click(object sender, EventArgs e)
